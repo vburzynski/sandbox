a8d1b422f2dbb308653a5b6b7c4b962aa8c5f76f
spinningtopsofdoom + Valerie Burzynski <dev+spinningtopsofdoom+vburzynski@bendyworks.com>
2018-04-20 10:44:41 -0500

    setup activeadmin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406215523) do
+ActiveRecord::Schema.define(version: 20180420154242) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
